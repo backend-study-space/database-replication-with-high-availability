3c97231450476d2b3b30ed44906186dab28e51cd
audghks1996 <audghks1996@naver.com>
2024-06-09 15:20:41 +0900

    Initial commit

---
+++ src/main/java/com/example/proxysql_ha/ProxysqlHaApplication.java
@@ -0,0 +1,13 @@
+package com.example.proxysql_ha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxysqlHaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxysqlHaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proxysql_ha/ProxysqlHaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proxysql_ha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxysqlHaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
