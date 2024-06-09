c1c1893dcc269a592edc72d99fa26096697b339d
audghks1996 <audghks1996@naver.com>
2024-06-09 17:50:00 +0900

    모듈 분리

--- sample-api/src/main/java/com/example/proxysql/ha/ProxysqlHaApplication.java
+++ sample-api/src/main/java/com/example/proxysql/ha/ProxysqlHaApplication.java
@@ -1,4 +1,4 @@
-package com.example.proxysql_ha;
+package com.example.proxysql.ha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
